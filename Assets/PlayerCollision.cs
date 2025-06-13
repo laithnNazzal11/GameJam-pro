9a5fbf49ee9cb21c0c02309fa56028ca71a4f25f
Mohamed Mansour <72463762+mman9our@users.noreply.github.com>
2025-06-13 16:18:14 +0300

    add animlas to the map

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            HealthManager.health--;
+            if(HealthManager.health <=0){
+                // PlayerManager.isGameOver = true;
+                // AudioManager.instance.Play("GameOver");
+                gameObject.SetActive(false);
+            }else{
+                StartCoroutine(GetHurt());
+            }
+        }
+    }
+    private Animator animator;
+    
+    private void Start()
+    {
+        // Cache the animator reference
+        animator = GetComponent<Animator>();
+    }
+    
+    IEnumerator GetHurt(){ 
+        Physics2D.IgnoreLayerCollision(6,8);
+        
+        // Only try to use the animator if it exists
+        if (animator != null)
+        {
+            animator.SetLayerWeight(1, 1);
+        }
+        
+        yield return new WaitForSeconds(3);
+        
+        // Only try to use the animator if it exists
+        if (animator != null)
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+        
+        Physics2D.IgnoreLayerCollision(6,8, false);
+    }
+}
