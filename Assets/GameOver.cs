0a43dad0ba28595c99d64e7ccc1213c6b7211d7f
Mohamed Mansour <72463762+mman9our@users.noreply.github.com>
2025-06-14 06:30:24 +0300

    refactor: remove sound effects from GameOver collision handling

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -3,20 +3,10 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-    private SoundEffectsLayer soundEffects;
     [SerializeField] private GameObject gameOverPanel; 
       
-    void Start()
-    {
-        soundEffects = FindObjectOfType<SoundEffectsLayer>();
-    }
-    
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (soundEffects != null)
-        {
-            soundEffects.PlaySFX(soundEffects.winSound);
-        }
         if (gameOverPanel != null)
         {
             HealthManager.health = 0;
